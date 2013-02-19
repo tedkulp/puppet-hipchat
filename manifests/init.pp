9d6aab6e41b0f8a461667bb9e7a7e21d73d54661
Ted Kulp <ted@tedkulp.com>
2013-02-19 06:05:55 -0500

    Added init.pp.  Updated README.

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+# Public: Installs the Hipchat app
+#
+# Example
+#
+#   include hipchat
+
+class hipchat {
+  package { 'Hipchat':
+    provider => 'appdmg',
+    source   => 'http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.19-01uennzomg01wmg.zip'
+  }
+}
